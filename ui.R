918c3b8d2d7edfefccee080a433c0edbdc42154b
Ubuntu <david.i.chan@gmail.com>
2017-01-04 04:51:33 +0000

    Added submit button to UI.

--- ui.R
+++ ui.R
@@ -22,7 +22,9 @@ shinyUI <- basicPage(
     sidebarPanel("",
       textInput("myselect", "Enter a twitter username (no @)", "neiltyson"),
       #verbatimTextOutput("value")
-      tableOutput("values")),
+      tableOutput("values"),
+      submitButton("Run Query")
+),
 
     mainPanel(
       helpText("See where your twitter account lies on the ideological spectrum"),
@@ -32,7 +34,7 @@ shinyUI <- basicPage(
         tabPanel("Twitter elites to consider following", dataTableOutput("elitestable")),
         tabPanel("Some twitter mortals to consider following", dataTableOutput("mytable"))),
       helpText("Built using the ", a("tweetscores package", href="https://github.com/pablobarbera/twitter_ideology")),
-      helpText("Questions? Tweet me ", a("@tweetertotterxy", href="https://twitter.com/tweetertotterxy"))
+      helpText("Questions? Tweet me ", a("@dch4n", href="https://twitter.com/dch4n"))
           )
   )
 )
